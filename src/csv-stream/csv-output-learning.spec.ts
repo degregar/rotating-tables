171ff10d59ddcb5f85cc3d9da3e438f8fdb8ce8e
Michał Kukla <michalkukla@Michas-MacBook-Pro.local>
2023-01-27 16:39:29 +0100

    test: learn how to format csv output

--- src/csv-stream/csv-output-learning.spec.ts
+++ src/csv-stream/csv-output-learning.spec.ts
@@ -13,4 +13,16 @@ describe("CSV Output learning tests", () => {
     csvStream.write({ id: "9", json: "[]", is_valid: "false" });
     csvStream.end();
   });
+
+  it("should print single element or no element arrays with quotes", () => {
+    // given
+    const csvStream = format({ headers: true, quoteColumns: [false, true, false], quoteHeaders: false });
+
+    // when
+    csvStream.pipe(process.stdout);
+    csvStream.write({ id: "3", json: "[-5,-5]", is_valid: "true" });
+    csvStream.write({ id: "9", json: "[-5]", is_valid: "false" });
+    csvStream.write({ id: "9", json: "[]", is_valid: "false" });
+    csvStream.end();
+  });
 });
