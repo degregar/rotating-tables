da405c2cb524d41d2ec6eef3cdcf6f8b8b760fbe
Michał Kukla <michalkukla@Michas-MacBook-Pro.local>
2023-01-27 12:11:41 +0100

    test: add test data

---
+++ src/tests/test-data.ts
@@ -0,0 +1,7 @@
+const arrayFrom = (n: number) => Array.from(Array(n).keys()).map((x) => x + 1);
+
+export const twoByTwoTable = arrayFrom(2 * 2);
+export const threeByThreeTable = arrayFrom(3 * 3);
+export const fourByFourTable = arrayFrom(4 * 4);
+export const fiveByFiveTable = arrayFrom(5 * 5);
+export const sixBySixTable = arrayFrom(6 * 6);
