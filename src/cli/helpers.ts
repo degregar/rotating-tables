e7a9b735e1fa268de0f34b0064520342c0ad0459
Michał Kukla <michalkukla@Michas-MacBook-Pro.local>
2023-01-30 09:25:07 +0100

    refactor: move out helpers from the main file

--- src/cli/helpers.ts
+++ src/cli/helpers.ts
@@ -1,3 +1,7 @@
+import { format } from "@fast-csv/format";
+const csv = require("csv-stream");
+const fs = require("fs");
+
 import { FlatTable } from "../flat-table/flat-table";
 import { InvalidInputError } from "../errors";
 import { SubstituteTableRotator } from "../substitute-table-rotator/substitute-table-rotator";
@@ -40,4 +44,55 @@ export type InputRow = {
 
 export const isInputRow = (row: any): row is InputRow => {
   return !!row.id && !!row.json;
+};
+
+export function createCsvOutputStream() {
+  return format({
+    headers: ["id", "json", "is_valid"],
+    quoteColumns: [false, true, false],
+    quoteHeaders: false,
+  });
+}
+
+export function assertFileExists(inputPath: string) {
+  if (!fs.existsSync(inputPath)) {
+    throw new Error("Input file does not exist");
+  }
+}
+
+export function createCsvInputStream() {
+  const csvInputStream = csv.createStream({
+    columns: ["id", "json"],
+    endLine: "\n",
+    escapeChar: '"',
+    enclosedChar: '"',
+  });
+  return csvInputStream;
+}
+
+export const transform = (inputPath: string) => {
+  const csvOutputStream = createCsvOutputStream();
+  csvOutputStream.pipe(process.stdout);
+
+  try {
+    assertFileExists(inputPath);
+
+    const csvInputStream = createCsvInputStream();
+
+    fs.createReadStream(inputPath)
+      .pipe(csvInputStream)
+      .on("data", (data: any) => {
+        if (!isHeaderRow(data)) {
+          const output = handleRow(data);
+          csvOutputStream.write(output, "utf-8");
+        }
+      })
+      .on("end", () => {
+        csvOutputStream.end();
+      });
+  } catch (error) {
+    csvOutputStream.end();
+    console.error(error);
+    process.exit(1);
+  }
 };
